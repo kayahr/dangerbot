a9745d55a790b4293135ebefdf07e4896b2f4b12
Benjamin P. Jung <headcr4sh@gmail.com>
2020-10-26 23:34:55 +0100

    Initial commit.

---
+++ index.ts
@@ -0,0 +1,18 @@
+import * as discord from "discord.js";
+
+async function main() {
+    const client = new discord.Client();
+    client.on("ready", () => {
+        console.log(`Logged in as ${client?.user?.tag}!`);
+    });
+    client.on('message', msg => {
+        if (msg.content.indexOf("ping") !== -1) {
+          msg.reply("pong");
+        }
+    });
+    await client.login(process.env["DANGERBOT_DISCORD_TOKEN"]);
+}
+
+(async () => {
+    await main();
+})();
